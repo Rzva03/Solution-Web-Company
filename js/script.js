1ae6ff1e25426871b9fa08835540b910e8efe574
Abraham Víctor Zaragoza Rodríguez <avictor.zrodriguez@gmail.com>
2021-09-30 21:38:51 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,79 @@
-const hamburger=document.querySelector(".hamburger");const navMenu=document.querySelector(".nav-menu");hamburger.addEventListener("click",mobileMenu);function mobileMenu(){hamburger.classList.toggle("active");navMenu.classList.toggle("active");} const navLink=document.querySelectorAll(".nav-link");navLink.forEach((n)=>n.addEventListener("click",closeMenu));function closeMenu(){hamburger.classList.remove("active");navMenu.classList.remove("active");} consoleText(["Bienvenido a Solution Web Company","Tecnología avanzada para el desarrollo de su empresa.",],"text",["white","white","white"]);function consoleText(words,id,colors){if(colors===undefined)colors=["#fff"];var visible=true;var con=document.getElementById("console");var letterCount=1;var x=1;var waiting=false;var target=document.getElementById(id);target.setAttribute("style","color:"+colors[0]);window.setInterval(function(){if(letterCount===0&&waiting===false){waiting=true;target.innerHTML=words[0].substring(0,letterCount);window.setTimeout(function(){var usedColor=colors.shift();colors.push(usedColor);var usedWord=words.shift();words.push(usedWord);x=1;target.setAttribute("style","color:"+colors[0]);letterCount+=x;waiting=false;},1000);}else if(letterCount===words[0].length+1&&waiting===false){waiting=true;window.setTimeout(function(){x=-1;letterCount+=x;waiting=false;},1000);}else if(waiting===false){target.innerHTML=words[0].substring(0,letterCount);letterCount+=x;}},120);window.setInterval(function(){if(visible===true){con.className="console-underscore hidden";visible=false;}else{con.className="console-underscore";visible=true;}},400);}
+// Hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+consoleText(
+  [
+    "Bienvenido a Solution Web Company",
+    "Tecnología avanzada para el desarrollo de su empresa.",
+  ],
+  "text",
+  ["white", "white", "white"]
+);
+// Console Text
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ["#fff"];
+  var visible = true;
+  var con = document.getElementById("console");
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id);
+  target.setAttribute("style", "color:" + colors[0]);
+  window.setInterval(function () {
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount);
+      window.setTimeout(function () {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute("style", "color:" + colors[0]);
+        letterCount += x;
+        waiting = false;
+      }, 1000);
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function () {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000);
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount);
+      letterCount += x;
+    }
+  }, 120);
+  window.setInterval(function () {
+    if (visible === true) {
+      con.className = "console-underscore hidden";
+      visible = false;
+    } else {
+      con.className = "console-underscore";
+      visible = true;
+    }
+  }, 400);
+}
+// Parallax efect
+// let parallax = document.querySelector(".parallax");
+
+// function scrollParallax() {
+//   let scrollTop = document.documentElement.scrollTop;
+//   parallax.style.transform = "translateY(" + scrollTop * -0.5 + "px)";
+// }
+
+// window.addEventListener("scroll", scrollParallax);
